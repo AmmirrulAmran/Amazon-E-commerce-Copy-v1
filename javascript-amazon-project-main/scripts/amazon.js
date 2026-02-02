21d92d69c653f88f86efdd4a430ea5e3f2686b2c
iFlish <ammirruladam@gmail.com>
2026-02-02 10:45:23 +0800

    Move addToCart to cart.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,5 @@
-import {cart} from '../data/cart.js'
-import {products} from '../data/products.js'
+import { cart,addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 let productsHTML = "";
 
@@ -68,37 +68,23 @@ products.forEach((product) => {
 
   //Loop through each button
 
+
+
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
+
   document.querySelectorAll(".js-add-to-cart").forEach((buttonElement) => {
     buttonElement.addEventListener("click", () => {
       const productId = buttonElement.dataset.productId;
-
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1,
-        });
-      }
-
-      let cartQuantity = 0;
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+      addToCart(productId);
+      updateCartQuantity();
     });
 
-   
-    
-    console.log(cart);
   });
 });
