7755e3b337b447948d1174c6c31761c659e99eae
Ammirrul Adam <ammirruladam@gmail.com>
2026-02-14 09:30:10 +0800

    Create class version of cart

---
+++ javascript-amazon-project-main/data/cart-class.js
@@ -0,0 +1,91 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+   
+  }
+
+  loadFromStorage = function () {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    //if cart empty
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 3,
+          deliveryOptionId: "2",
+        },
+      ];
+    }
+  };
+
+  saveToStorage = function () {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  };
+
+  addToCart(productId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: "1",
+      });
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart = function (productId) {
+    const newCart = [];
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+
+    console.log(cart);
+
+    this.saveToStorage();
+  };
+
+  updateDeliveryOption = function (productId, deliveryOptionId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  };
+}
+
+const cart = new Cart("cart-oop");
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
